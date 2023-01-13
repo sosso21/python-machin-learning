195ba44d503717223b398f34cd4ce8470f926388
sosso21 <sofianetop21.st@gmail.com>
2023-01-13 16:20:03 +0100

    :fire: (cour 06): brodcasting - 2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ A = np.random.randint(0, 10, [2, 2])
 
 B = np.random.randint(0, 10, [2, 2])
 
+# se the video https://www.youtube.com/watch?v=lIESSFHGalA&list=PLO_fdPEVlfKqMDNmCFzQISI2H_nJcEDJq&index=13
 AB = A + B
 
 print('AB : \n ', AB)
