b1ea1ce3c32ccc0496189c4942639bf63d036ad8
sosso21 <sofianetop21.st@gmail.com>
2023-01-14 06:40:55 +0100

    :construction: (coure 05): exercice

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import numpy as np
 
+np.random.seed(0)
+A = np. random.randint(0, 100, [10, 5])
+D = (A - np.mean(A, axis=0)) / A.std(axis=0)
 
-A = np.random.randint(0, 10, [2, 2])
 
-B = np.random.randint(0, 10, [2, 2])
-
-# se the video https://www.youtube.com/watch?v=lIESSFHGalA&list=PLO_fdPEVlfKqMDNmCFzQISI2H_nJcEDJq&index=13
-AB = A + B
-
-print('AB : \n ', AB)
+print("A : \n ", A)
+print("D : \n ", D)
+print("D.mean : \n ", D.mean(axis=0))
+print("D.std : \n ", D.std(axis=0))
