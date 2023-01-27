6abcf62caf0002accdef5f0506c77f188518d4b4
sosso21 <sofianetop21.st@gmail.com>
2023-01-27 16:55:04 +0100

    :construction: (cour 13 ): skitlean titanic

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
-iris = sns.load_dataset('iris')
-print("iris.head() : \n", iris.head())
+np.random.seed(0)
+m = 100  # creation de 100 échantillons
+X = np.linspace(0, 10, m).reshape(m, 1)
+y = X + np.random.randn(m, 1)
 
 
-sns.pairplot(iris, hue='species')
+model = LinearRegression()
+model.fit(X, y)  # entrainement du modele
+# évaluation avec le coefficient de corrélation
+print("score : ", model.score(X, y) * 100)
+
+
+plt.scatter(X, y)
+plt.plot(X, model.predict(X), c='red')
+
 plt.show()
 
+
+# lean with titanic
 
 titanic = sns.load_dataset('titanic')
-titanic.drop(['alone', 'alive', 'who', 'adult_male',
-             'embark_town', 'class'], axis=1, inplace=True)
+titanic = titanic[['survived', 'pclass', 'sex', 'age']]
 titanic.dropna(axis=0, inplace=True)
+titanic['sex'].replace(['female', 'male'], [0, 1], inplace=True)
+print("titanic : \n ", titanic.head())
 
-print("titanic.head() : \n", titanic.head())
-
-sns.catplot(x='survived', y='age', data=titanic, hue='sex')
-plt.show()
-
-sns.boxplot(x='survived', y='age', data=titanic, hue='sex')
-plt.show()
-
-# plt.figure(figsize=(8, 8))
-# sns.boxplot(x='age', y='fare', data=titanic, hue='sex')
-# plt.show()
+model = KNeighborsClassifier()
+y = titanic['survived']
+X = titanic.drop('survived', axis=1)
+model.fit(X, y)  # entrainement du modele
+print("score: ", model.score(X, y))  # évaluation
 
 
-sns.jointplot(x='age', y='fare', data=titanic, kind="hex")
+def survie(model, pclass=3, sex=1, age=24):
+    x = np.array([pclass, sex, age]).reshape(1, 3)
+    print("predict : \n", model.predict(x))
+    print("predict_proba : \n", model.predict_proba(x))
 
-plt.show()
 
-sns.heatmap(titanic.corr())
-plt.show()
+survie(model)
+
+# woman like Zhu
+survie(model, pclass=2, sex=0, age=27)
