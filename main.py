16d52a02593470ef53fc6f089b60e4dd26bc4fdc
sosso21 <sofianetop21.st@gmail.com>
2023-02-13 04:26:03 +0100

    :art: (cour15): boston

--- main.py
+++ main.py
@@ -1,85 +1,96 @@
+# Séparation des données en jeux d'entraînement et de test
 from sklearn.model_selection import learning_curve
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import validation_curve
-from sklearn.model_selection import cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split, GridSearchCV
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
+import seaborn as sns
+import pandas as pd
 
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-
-print("X: \n", X.shape)
-
-plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.8)
-plt.show()
-# learn
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2)
-
-print('Train set:', X_train.shape)
-print('Test set:', X_test.shape)
-
-plt.figure(figsize=(12, 4))
-plt.subplot(121)
-plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, alpha=0.8)
-plt.title('Train set')
-plt.subplot(122)
-plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, alpha=0.8)
-plt.title('Test set')
-
-plt.show()
+# Load the Boston Housing data from a CSV file
+boston = pd.read_csv("Boston.csv")
 
 
-model = KNeighborsClassifier(n_neighbors=1)
+X_train, X_test, y_train, y_test = train_test_split(boston.drop(
+    "medv", axis=1), boston["medv"], test_size=0.2)
 
-model.fit(X_train, y_train)
+# Séparation du jeu d'entraînement en jeux d'entraînement et de validation
+# we can add  random_state=0
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, y_train, test_size=0.2)
 
-print('train score:', model.score(X_train, y_train))
-print('test score:', model.score(X_test, y_test))
-
-# VALIDATION Set
-model = KNeighborsClassifier(10)
-cross_validation_mean = cross_val_score(
-    model, X_train, y_train, cv=5, scoring='accuracy').mean()
-print("cross_validation", cross_validation_mean)
-print("================================================")
+# Définition de la grille de paramètres à parcourir
+param_grid = [{'n_neighbors': range(1, 30),               'weights': ['uniform', 'distance'],
+               'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+               'p': range(1, 5),
+               'leaf_size':  range(0, 50)
+               }
+              ]
 
 
-# trouver le meilleur KNeighborsClassifier
-
-param_grid = {'n_neighbors': np.arange(1, 20),
-              'metric': ['euclidean', 'manhattan']}
-
-grid = GridSearchCV(KNeighborsClassifier(), param_grid, cv=5)
-
+# Entraînement du modèle KNeighborsRegressor avec GridSearchCV
+grid = GridSearchCV(KNeighborsRegressor(), param_grid, cv=5)
 grid.fit(X_train, y_train)
-
-
-print("best_score_ : \n ", grid.best_score_)
-print("best_params_ : \n ", grid.best_params_)
-
 model = grid.best_estimator_
 
-print("model.score : \n ", model.score(X_test, y_test))
+
+# Prédiction sur les données de validation
+y_val_pred = model.predict(X_val)
+
+# Calcul des métriques d'évaluation sur les données de validation
+mae = mean_absolute_error(y_val, y_val_pred)
+mse = mean_squared_error(y_val, y_val_pred)
+r2 = r2_score(y_val, y_val_pred)
+
+# Affichage des métriques d'évaluation sur les données de validation
+print("Métriques sur le jeu de validation:")
+print("Erreur absolue moyenne: ", mae)
+print("Erreur quadratique moyenne: ", mse)
+print("Score R^2: ", r2)
+print("\n \n ")
+
+# Tracé des métriques d'évaluation sur les données de validation
+
+plt.subplot(1, 2, 1)
+sns.scatterplot(x=y_val, y=y_val_pred)
+sns.lineplot(x=y_val, y=y_val, c="red", label="Valeurs réelles")
+plt.xlabel("Valeurs réelles")
+plt.ylabel("Valeurs prédites")
+plt.title("Métriques d'évaluation sur le jeu de validation")
 
 
-print("confusion_matrix : \n ", confusion_matrix(y_test, model.predict(X_test)))
+# Prédiction sur les données de test
+y_test_pred = model.predict(X_test)
 
-print("======== courbe d'apprentissage =============")
+# Calcul des métriques d'évaluation sur les données de test
+mae = mean_absolute_error(y_test, y_test_pred)
+mse = mean_squared_error(y_test, y_test_pred)
+r2 = r2_score(y_test, y_test_pred)
 
-N, train_score, val_score = learning_curve(
-    model, X_train, y_train, train_sizes=np.linspace(0.1, 1, 10), cv=5)
+# Affichage des métriques d'évaluation sur les données de test
+print("Métriques sur le jeu de test:")
+print("Erreur absolue moyenne: ", mae)
+print("Erreur quadratique moyenne: ", mse)
+print("Score R^2: ", r2)
+print("\n \n ")
 
-print("N : ", N)
-plt.plot(N, train_score.mean(axis=1), label='train')
-plt.plot(N, val_score.mean(axis=1), label='validation')
-plt.xlabel('train_sizes')
-plt.legend()
+
+print("Meilleure combinaison de paramètres trouvée: ", grid.best_params_)
+print("Meilleure Score : ", grid.best_score_)
+
+# Tracé des métriques d'évaluation sur les données de test
+
+plt.subplot(1, 2, 2)
+
+sns.scatterplot(x=y_test, y=y_test_pred)
+sns.lineplot(x=y_val, y=y_val, c="red", label="Valeurs réelles")
+
+plt.xlabel("Valeurs réelles")
+plt.ylabel("Valeurs prédites")
+plt.title("Métriques d'évaluation sur le jeu de test")
+
+
 plt.show()
+plt.savefig('figure.png')
